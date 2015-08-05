a3ed25888c8feaa7058d7f8ec05dcd02e2c026d5
markdalgleish <mark.john.dalgleish@gmail.com>
2015-08-06 08:50:54 +1000

    Update reducer path in createStore

--- src/flux/createStore.js
+++ src/flux/createStore.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 
-import reducer from './reducer';
+import reducer from './reducers/reducer';
 
 export default (data) => {
   return createStore(reducer, data);
